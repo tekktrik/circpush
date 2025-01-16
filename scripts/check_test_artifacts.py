ac72e19324a94e0a44b8ad051f85c9df90e097f7
Alec Delaney <tekktrik@gmail.com>
2025-01-15 22:05:19 -0500

    Add Python script for checking for test artifacts

---
+++ scripts/check_test_artifacts.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2025 Alec Delaney
+#
+# SPDX-License-Identifier: MIT
+
+"""Cross-platform script for getting the application configuration directory
+
+Author(s): Alec Delaney
+"""
+
+# pragma: no cover
+
+import pathlib
+import sys
+
+import click
+
+test_config = click.get_app_dir(".circpush-test")
+if pathlib.Path(test_config).exists:
+    print("Old test artifact exists, please remove before running tests.")
+    print("Note that old workspaces and other information may be contained in the folder.")
+    print("(You can run make wipe-test-artiacts to remove the test artifacts)")
+    sys.exit(1)
